4289dcc0451067159869fedad2648f6d9a523b30
Neha Varma <varmaneha820@gmail.com>
2025-01-13 18:51:28 +0530

    Calculator_Project added

---
+++ The_Calculator_Project.py
@@ -0,0 +1,42 @@
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+" : add,
+    "-" : subtract,
+    "*" : multiply,
+    "/" : divide,
+    }
+
+#print(operations["*"](4, 8))
+def calculator():
+    should_accumulate = True
+    num1 = float(input("What is the first number? "))
+
+    while should_accumulate:
+        for symbol in operations:
+            print(symbol)
+        opeartion_symbol = input("Pick an operation: ")
+        num2 = float(input("What is the next number? "))
+        answer = (operations[opeartion_symbol](num1, num2))
+        print(f"{num1} {opeartion_symbol} {num2} = {answer} ")
+
+        choice = input(f"Type 'y' to continue calculation {answer}, or type 'n' to start a new calculation: ")
+        
+        if choice == "y":
+            num1 = answer
+        else:
+            should_accumulate = False
+            print("\n" * 20)
+            calculator()
+            
+calculator()
