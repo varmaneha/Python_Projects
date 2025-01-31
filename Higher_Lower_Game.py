3f176c292e645ac785b57f73659c9490f6527b6f
Neha Varma <varmaneha820@gmail.com>
2025-01-31 17:35:01 +0530

    Modified Project

--- Higher_Lower_Game.py
+++ Higher_Lower_Game.py
@@ -16,29 +16,36 @@ def check_answer(user_guess, a_followers, b_followers):
     
 print(logo)
 score = 0
-
-account_a = random.choice(data)
+game_should_continue = True
 account_b = random.choice(data)
-if account_a == account_b:
+
+while game_should_continue:
+    account_a = account_b
     account_b = random.choice(data)
+    if account_a == account_b:
+        account_b = random.choice(data)
 
 
-print(f"Compare A: {format_data(account_a)}.")
-print(vs)
-print(f"Compare B: {format_data(account_b)}.")
+    print(f"Compare A: {format_data(account_a)}.")
+    print(vs)
+    print(f"Compare B: {format_data(account_b)}.")
 
-guess = input("Who has more followers? Type 'A' or 'B':").lower()
+    guess = input("Who has more followers? Type 'A' or 'B':").lower()
+    
+    print("\n" * 20)
+    print(logo)
 
-a_follower_count = account_a["follower_count"]
-b_follower_count = account_b["follower_count"]
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
 
-is_correct = check_answer(guess, a_follower_count, b_follower_count)
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
 
-if is_correct:
-    score += 1
-    print(f"You are right! Current score {score}")
-else:
-    print(f"Sorry, thats wrong: Final score {score}")
+    if is_correct:
+        score += 1
+        print(f"You are right! Current score {score}")
+    else:
+        print(f"Sorry, thats wrong: Final score {score}")
+        game_should_continue = False
 
 
 
